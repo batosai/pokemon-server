1677af1fc72ae7e409b3dfd8725cb09b045d6afd
jeremy <jeremy@chaufourier.fr>
2020-03-22 00:11:28 +0100

    create ln upload

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:batosai/pokemon-server.git',
       path : '/app/server',
-      'post-deploy' : 'npm install && npm audit fix && npx pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && npm audit fix && ln -s /app/server/shared/uploads/ /app/server/source/public/uploads && npx pm2 reload ecosystem.config.js --env production'
     }
   }
 };
